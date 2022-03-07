1fbd1097bd895fb03af9c88662c09efd3b96e937
djtoler <84260551+djtoler@users.noreply.github.com>
2022-03-07 11:38:36 -0500

    all 3 working

--- apps/portfolio/pages/index.tsx
+++ apps/portfolio/pages/index.tsx
@@ -1,12 +1,12 @@
-import { Button } from "ui";
-import {Button2} from "../../../components/Button2";
+import { Button} from "ui";
+// import {Primary } from "../../../stories/Button.stories"
 
 export default function Web() {
   return (
     <div>
       <h1>Portfolio</h1>
       <Button />
-      <Button2 variant="secondary" children={""}/>
+      
     </div>
   );
 }
